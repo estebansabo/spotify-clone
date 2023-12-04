1d008df7994f95a6ee3b7485949c67db1c957bae
Esteban Sabo G <estebanzsabo@gmail.com>
2023-12-04 16:00:55 -0400

    Button done

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
+import Button from "./Button";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -37,6 +38,20 @@ const Header: React.FC<HeaderProps> = ({
                     <button className="rounded-full p-2 bg-white flex items-center justify-center cursor-pointer hover:opacity-75 transition">
                         <BiSearch className="text-black" size={20}/>
                     </button>
+                </div>
+                <div className="flex justify-between items-center gap-x-4">
+                    <>
+                        <div>
+                            <Button className="bg-transparent text-neutral-300 font-medium">
+                            Sign up
+                            </Button>
+                        </div>
+                        <div>
+                            <Button className="bg-white px-6 py-2">
+                            Log in
+                            </Button>
+                        </div>
+                    </>
                 </div>
             </div>
         </div>
